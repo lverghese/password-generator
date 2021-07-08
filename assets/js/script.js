19d49c8d82cec353d1bc78dbd35fa5cbba98a55a
unknown <loganverghese@gmail.com>
2021-07-07 21:39:03 -0400

    initial prompt question

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,16 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+    
+    var passwordLength = window.prompt("How long do you want your password? (8-128) characters");
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please try again! You must choose a number between 8 and 128!");
+    return 0;
+  } 
+
+
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
