35496bc5984d3b0bedc33ebb096f5161f0f2e094
unknown <loganverghese@gmail.com>
2021-07-07 21:41:32 -0400

    initial function setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(passwordLength, passwordLower, passwordUpper, passwordNumbers, passwordSpecial) {
     
     var passwordLength = window.prompt("How long do you want your password? (8-128) characters");
   if (passwordLength < 8 || passwordLength > 128) {
@@ -19,7 +19,7 @@ function writePassword() {
  var passwordSpecial = window.confirm("Do you want special characters?");
 
 
-  var password = generatePassword();
+  var password = generatePassword(passwordLength, passwordLower, passwordUpper, passwordNumbers, passwordSpecial);
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 
